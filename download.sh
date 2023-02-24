45c3d8749bdaabc86c5b6f194a97c674a9e03778
Christopher Woodall <woodall.christopher@gmail.com>
2023-02-24 16:43:49 -0500

    adds parameter substitution to `download.sh`
    Allows `PRESIGNED_URL` and `TARGET_FOLDER` to be declared as environmental variables.

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
-***REMOVED***=""             # replace with presigned url from email
-MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
-TARGET_FOLDER=""             # where all files should end up
+***REMOVED***="${***REMOVED***:-}"  # replace with presigned url from email
+MODEL_SIZE="7B,13B,30B,65B"         # edit this list with the model sizes you wish to download
+TARGET_FOLDER="${TARGET_FOLDER:-}"  # where all files should end up
 
 declare -A N_SHARD_DICT
 
